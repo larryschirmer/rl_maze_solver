dd32dcdc945c374a19891bfaf0f252bd947deb7a
Larry Schirmer <larryschirmer@gmail.com>
2019-04-13 15:24:07 -0400

    add reward function to agent
    initialize the reward dictionary with random numbers
    add the reward function to the learn method

--- agent.py
+++ agent.py
@@ -2,14 +2,27 @@ import numpy as np
 
 
 class Agent(object):
-    def __init__(self):
+    def __init__(self, states, alpha):
         self.stateHistory = None
+        self.alpha = alpha
+        self.G = {}
+        self.initReward(states)
+
+    def initReward(self, states):
+        for state in states:
+            self.G[state] = np.random.uniform(low=-1.0, high=1.0)
+
+    def learn(self):
+        target = 0
+
+        for prev, reward in reversed(self.stateHistory):
+            self.G[prev] = self.G[prev] + self.alpha * (target - self.G[prev])
+            target += reward
+        
+        self.stateHistory = []
 
     def chooseAction(self):
         pass
 
     def updateStateHistory(self):
-        pass
-
-    def learn(self):
         pass
